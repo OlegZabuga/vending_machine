9517cde277b0aebaed540fff286b9e96830a4d20
Oleg Zabuga <7vb3msb@mail.ru>
2024-05-28 19:51:56 +0300

    Изменить структуру проекта(добавить папки core и apps

--- core/apps/api/urls.py
+++ core/apps/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api import views
+from core.apps.api import views
 
 router = DefaultRouter()
 router.register(r'goods', views.GoodViewSet)
